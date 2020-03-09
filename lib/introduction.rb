a0e34196119843b34a955ee200d6a0870a19de4a
galo velastegui <dvelastegui@yahoo.com>
2020-03-09 01:04:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,12 @@
 # lib/introduction.rb
  
 def introduction(name)
+<<<<<<< HEAD
   puts "Hi, my name is #{name}."
   def introduction_with_language(name, language)
     puts "Hi, my name is #{name} and I am learning to program in #{language}."
   end
+=======
+  puts "Hi, my name is #{name}." 
+>>>>>>> 6b9b90c5c6184c84464ea618253e7249e37acf1d
 end
